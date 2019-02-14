0b2bf4221330693653c41c3a3e74453a6992bab4
Wyatt Whorton <bluebomber019@gmail.com>
2019-02-13 18:34:38 -0700

    Minor Fix
    Made easier for Connor

--- api/routes/DeveloperRoute.js
+++ api/routes/DeveloperRoute.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
     .get(Developer.list_all_dev_tweaks)
     app.route('/dev/:devName/:tweakName')
     .get(Developer.list_tweak)
-  app.get('/:dev/:tweak/:version/:filename', function (req, res) {
+  app.get('/dev/:dev/:tweak/:version/:filename', function (req, res) {
     var filePath = "./api/public/developers/"+req.params.dev+"/"+req.params.tweak+"/"+req.params.version+"/"+req.params.filename;
     var fileName = "1.zip"; 
     res.download(filePath, fileName); 

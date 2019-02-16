4a8d0a1cee3be12885c8720fc3564fb24e517001
Wyatt Whorton <bluebomber019@gmail.com>
2019-02-15 19:10:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 var express = require('express');
-const fs = require('fs');
 const https = require('https');
+const fs = require('fs');
 
   app = express(),
-  port = process.env.PORT || 443,
+  port = process.env.PORT || 8443,
   bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var routes = require('./api/routes/DeveloperRoute.js'); 
 routes(app);
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/dev.mintdev.co/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/dev.mintdev.co/cert.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/dev.mintdev.co/cert.pem', 'utf8');
 const ca = fs.readFileSync('/etc/letsencrypt/live/dev.mintdev.co/chain.pem', 'utf8');
 
 const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
+        key: privateKey,
+        cert: certificate,
+        ca: ca
 };
-
-httpsServer.listen(443, () => {
-	console.log('HTTPS Server running on port 443');
-});
 const httpsServer = https.createServer(credentials, app);
-
+httpsServer.listen(8443, () => {
+        console.log('HTTPS Server running on port 8443');
+});
 console.log('Packager RESTful API server started on: https');
